55b7680570a6b1443de38e2f0fdec51369595a7b
duynn5 <v.duynn5@vinid.net>
2019-08-09 12:08:13 +0700

    update be url

--- app/constants/service-model.js
+++ app/constants/service-model.js
@@ -1,5 +1,5 @@
-// export const ROOT = 'http://108.59.85.23/api/v1';
-export const ROOT = 'http://localhost:8080/api/v1';
+export const ROOT = 'http://http://35.202.4.202/api/v1';
+// export const ROOT = 'http://localhost:8080/api/v1';
 
 export const CATEGORIES = `${ROOT}/categories`;
 export const CHILD_CATEGORY = `${ROOT}/categories/child`;
@@ -21,7 +21,3 @@ export const REPORTS = `${ROOT}/reports`;
 export const REPORTS_PDF = `${ROOT}/reports/pdf`;
 export const REPORTS_JSON = `${ROOT}/reports/json`;
 export const LOGIN_URL = `${ROOT}/authentication`;
-//
-// export const GET_EXAM_LIST = `${ROOT}/list`;
-// export const GET_EXAM = `${ROOT}/{exam}`;
-// export const POST_EXAM = `${ROOT}/{exam}`;
